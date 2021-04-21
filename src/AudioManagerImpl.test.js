bf737060d095048ef90180b6f21d6bfd56603aed
Tony Atkins <tony@raisingthefloor.org>
2021-04-21 13:19:26 +0200

    Revert "C2LC-227: Fixed 'value' warnings in test output and disabled 'note tuning' diagram."
    This reverts commit 891ff6a618f49d761d555cc404924037767a2c6e.

--- src/AudioManagerImpl.test.js
+++ src/AudioManagerImpl.test.js
@@ -5,37 +5,36 @@ import CharacterState from './CharacterState';
 import SceneDimensions from './SceneDimensions';
 import {Frequency} from 'tone';
 
-// Uncomment these functions (and the call below to output the current tuning to the console when the tests run.)
-// function arrayToPaddedRowString (array: Array<any>) : string {
-//     const paddedArray = [];
-//     array.forEach((item) => {
-//         paddedArray.push(item.toString().padStart(5, " "));
-//     });
-//     return "| " + paddedArray.join(" | ") + " |";
-// }
-//
-// function logTuning (noteTable: Array<Array<string>>) {
-//     const tableStringSegments = [];
-//     // Column Headings
-//     const colHeadings = ["", "A -3", "A -2", "A -1", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "Q +1", "Q +2", "Q +3"];
-//     tableStringSegments.push(arrayToPaddedRowString(colHeadings));
-//     // GfM table syntax.
-//     const tableDividers = new Array(colHeadings.length);
-//     tableDividers.fill("-----");
-//     tableStringSegments.push(arrayToPaddedRowString(tableDividers));
+function arrayToPaddedRowString (array: Array<any>) : string {
+    const paddedArray = [];
+    array.forEach((item) => {
+        paddedArray.push(item.toString().padStart(5, " "));
+    });
+    return "| " + paddedArray.join(" | ") + " |";
+}
 
-//     for (let row = 0; row < noteTable.length; row++) {
-//         const rowEntries = noteTable[row];
-//         // Row Heading
-//         const rowStringSegments = [ (row - 2) ];
-//         for (let col = 0; col < rowEntries.length; col ++) {
-//             const singleNote: string = rowEntries[col];
-//             rowStringSegments.push(singleNote);
-//         }
-//         tableStringSegments.push(arrayToPaddedRowString(rowStringSegments));
-//     }
-//     console.log(tableStringSegments.join("\n"));
-// }
+function logTuning (noteTable: Array<Array<string>>) {
+    const tableStringSegments = [];
+    // Column Headings
+    const colHeadings = ["", "A -3", "A -2", "A -1", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "Q +1", "Q +2", "Q +3"];
+    tableStringSegments.push(arrayToPaddedRowString(colHeadings));
+    // GfM table syntax.
+    const tableDividers = new Array(colHeadings.length);
+    tableDividers.fill("-----");
+    tableStringSegments.push(arrayToPaddedRowString(tableDividers));
+
+    for (let row = 0; row < noteTable.length; row++) {
+        const rowEntries = noteTable[row];
+        // Row Heading
+        const rowStringSegments = [ (row - 2) ];
+        for (let col = 0; col < rowEntries.length; col ++) {
+            const singleNote: string = rowEntries[col];
+            rowStringSegments.push(singleNote);
+        }
+        tableStringSegments.push(arrayToPaddedRowString(rowStringSegments));
+    }
+    console.log(tableStringSegments.join("\n"));
+}
 
 test("Returns a sensible note range for every supported character position.", () => {
     // 4 rows "in bounds" on either side of the centre plus testing three "out of bounds" rows on each side.
@@ -73,6 +72,5 @@ test("Returns a sensible note range for every supported character position.", ()
         expect(pitchRange).toBeLessThanOrEqual(12);
     }
 
-    // Uncomment this and the functions above to log the tuning as part of test runs.
-    // logTuning(noteTable);
+    logTuning(noteTable);
 });
