a935fffb3c4b0df3fccac5b11aaf1b75d648d7cc
Tony Atkins <tony@raisingthefloor.org>
2021-04-28 11:56:05 +0200

    C2LC-359: Updated to use active 'moving' announcement and 'assertive' aria-live region.

--- src/ProgramBlockEditor.js
+++ src/ProgramBlockEditor.js
@@ -590,12 +590,6 @@ class ProgramBlockEditor extends React.Component<ProgramBlockEditorProps, Progra
         )
     }
 
-    clearCharacterPositionAriaLive() {
-        const ariaLiveRegion = document.getElementById('character-position');
-        // $FlowFixMe: Flow doesn't know that elements have innerText.
-        ariaLiveRegion.innerText = "";
-    }
-
     getCharacterAriaLabel() {
         if (this.props.world === 'space') {
             return this.props.intl.formatMessage(
@@ -611,6 +605,18 @@ class ProgramBlockEditor extends React.Component<ProgramBlockEditorProps, Progra
             );
         }
     }
+
+    clearCharacterPositionAriaLive() {
+        const ariaLiveRegion = document.getElementById('character-position');
+        const character = this.getCharacterAriaLabel();
+
+        // $FlowFixMe: Flow doesn't know that elements have innerText.
+        ariaLiveRegion.innerText=this.props.intl.formatMessage(
+            {id:'ProgramBlockEditor.movementAriaLabel'},
+            { character }
+        );
+    }
+
 
     updateCharacterPositionAriaLive() {
         const characterState = this.props.characterState;
