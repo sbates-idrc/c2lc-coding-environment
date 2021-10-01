55827af4a50ffd9a1ce009f7a7c80ace7dbcb384
Tony Atkins <tony@raisingthefloor.org>
2021-10-01 11:34:09 +0200

    C2LC-489: Focus on 'OK' button when share confirmation modal opens.

--- src/ShareButton.js
+++ src/ShareButton.js
@@ -34,6 +34,11 @@ class ShareButton extends React.Component<ShareButtonProps, ShareButtonState> {
         // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText
         navigator.clipboard.writeText(currentUrl).then(() => {
             this.setState({ showShareComplete: true})
+
+            const confirmButtonRef = document.querySelector(".ShareCompleteModal button");
+            if(confirmButtonRef) {
+                confirmButtonRef.focus();
+            }
         });
     }
 
