b0b034a7eeb43934e94d067e24ec38fbea45311c
Tony Atkins <tony@raisingthefloor.org>
2021-11-23 15:30:43 +0100

    C2LC-453: Fixed classname with a space.

--- src/App.js
+++ src/App.js
@@ -1162,7 +1162,7 @@ export class App extends React.Component<AppProps, AppState> {
                             </IconButton>
                         </div>
 
-                        <div className='App__ PenDownToggleSwitch-container'>
+                        <div className='App__PenDownToggleSwitch-container'>
                             <PenDownToggleSwitch
                                 className='App__penDown-toggle-switch'
                                 value={this.state.drawingEnabled}
