7cb517cd4a31b2927be1b4d6059b50daf80995ee
Simon Bates <sbates@ocadu.ca>
2022-09-12 09:19:45 -0400

    NOJIRA: Set version to 1.6

--- src/App.js
+++ src/App.js
@@ -146,7 +146,7 @@ export class App extends React.Component<AppProps, AppState> {
     constructor(props: any) {
         super(props);
 
-        this.version = '1.5';
+        this.version = '1.6';
 
         this.appContext = {
             bluetoothApiIsAvailable: FeatureDetection.bluetoothApiIsAvailable()
