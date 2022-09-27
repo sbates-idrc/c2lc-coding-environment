d09262de9cf23d164f64c4b51ebc82524c08bf15
Simon Bates <sbates@ocadu.ca>
2022-09-27 13:40:28 -0400

    C2LC-672: Centre cookie notification and increase font size

--- src/CookieNotification.js
+++ src/CookieNotification.js
@@ -16,15 +16,17 @@ class CookieNotification extends React.Component<CookieNotificationProps, {}> {
                 <div className='CookieNotification__message'>
                     <FormattedMessage id='CookieNotification.message'/>
                 </div>
-                <button className='CookieNotification__button' onClick={this.props.onDismiss}>
-                    <FormattedMessage id='CookieNotification.dismiss'/>
-                </button>
-                <button
-                    className='CookieNotification__learnMoreButton CookieNotification__button'
-                    onClick={this.props.onLearnMore}
-                >
-                    <FormattedMessage id='CookieNotification.learnMore'/>
-                </button>
+                <div>
+                    <button className='CookieNotification__button' onClick={this.props.onDismiss}>
+                        <FormattedMessage id='CookieNotification.dismiss'/>
+                    </button>
+                    <button
+                        className='CookieNotification__button CookieNotification__button--margin-left CookieNotification__learnMoreButton'
+                        onClick={this.props.onLearnMore}
+                    >
+                        <FormattedMessage id='CookieNotification.learnMore'/>
+                    </button>
+                </div>
             </div>
         );
     }
