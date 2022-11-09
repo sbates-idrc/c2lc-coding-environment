607a25cd6da6991232c0b81b100bcfeb86ed5f5f
Simon Bates <sbates@ocadu.ca>
2022-11-09 15:18:32 -0500

    Refactor crickit_circuit_playround_bluefruit_dc.ino

--- open_robot_kit/crickit_circuit_playround_bluefruit_dc/crickit_circuit_playround_bluefruit_dc.ino
+++ open_robot_kit/crickit_circuit_playround_bluefruit_dc/crickit_circuit_playround_bluefruit_dc.ino
@@ -3,74 +3,32 @@
 #include <Adafruit_Crickit.h>
 #include <seesaw_motor.h>
 
-BLEService robotService("19B10000-E8F2-537E-4F6C-D104768A1214");
-BLECharacteristic commandCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214");
-BLECharacteristic notificationCharacteristic("19B10002-E8F2-537E-4F6C-D104768A1214");
+// Weavly robot protocol
+
+const char robotServiceUuid[] = "19B10000-E8F2-537E-4F6C-D104768A1214";
+const char commandCharacteristicUuid[] = "19B10001-E8F2-537E-4F6C-D104768A1214";
+const char notificationCharacteristicUuid[] = "19B10002-E8F2-537E-4F6C-D104768A1214";
+
+const uint8_t commandForward = 1;
+const uint8_t commandLeft = 3;
+const uint8_t commandRight = 2;
+
+const uint8_t notificationCommandFinished = 2;
+
+// Globals
+
+BLEService robotService(robotServiceUuid);
+BLECharacteristic commandCharacteristic(commandCharacteristicUuid);
+BLECharacteristic notificationCharacteristic(notificationCharacteristicUuid);
 
 Adafruit_Crickit crickit;
 
-seesaw_Motor motor_a(&crickit);
-seesaw_Motor motor_b(&crickit);
-
-void connect_callback(uint16_t conn_handle)
-{
-    (void) conn_handle;
-
-    Serial.println("Connected");
-}
-
-void disconnect_callback(uint16_t conn_handle, uint8_t reason)
-{
-    (void) conn_handle;
-    (void) reason;
-    
-    Serial.println("Disconnected");
-}
-
-void command_callback(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, uint16_t len)
-{
-    (void) conn_hdl;
-    (void) chr;
-
-    Serial.println("Command received");
-
-    if (len == 1) {
-        if (data[0] == 0x01) {
-            // Forward
-            notificationCharacteristic.notify8(0x01);
-            motor_a.throttle(0.3);
-            motor_b.throttle(0.3);
-            delay(800);
-            motor_a.throttle(0);
-            motor_b.throttle(0);
-            notificationCharacteristic.notify8(0x02);
-        } else if (data[0] == 0x03) {
-            // Left
-            notificationCharacteristic.notify8(0x01);
-            motor_a.throttle(-0.3);
-            motor_b.throttle(0.3);
-            delay(472);
-            motor_a.throttle(0);
-            motor_b.throttle(0);
-            notificationCharacteristic.notify8(0x02);
-        } else if (data[0] == 0x02) {
-            // Right
-            notificationCharacteristic.notify8(0x01);
-            motor_a.throttle(0.3);
-            motor_b.throttle(-0.3);
-            delay(472);
-            motor_a.throttle(0);
-            motor_b.throttle(0);
-            notificationCharacteristic.notify8(0x02);
-        }
-    }
-
-    Serial.println("Command completed");
-}
+seesaw_Motor leftMotor(&crickit);
+seesaw_Motor rightMotor(&crickit);
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial) {
         delay(10);
     }
@@ -93,35 +51,116 @@ void setup()
         Serial.println("Crickit started");
     }
 
-    // Attach motor a
-    motor_a.attach(CRICKIT_MOTOR_A1, CRICKIT_MOTOR_A2);
+    // Attach motors
+    leftMotor.attach(CRICKIT_MOTOR_A1, CRICKIT_MOTOR_A2);
+    rightMotor.attach(CRICKIT_MOTOR_B1, CRICKIT_MOTOR_B2);
 
-    // Attach motor b
-    motor_b.attach(CRICKIT_MOTOR_B1, CRICKIT_MOTOR_B2);
+    setupBluetooth();
+}
 
-    Bluefruit.begin();
+void setupBluetooth()
+{
+    if (!Bluefruit.begin()) {
+        Serial.println("Error starting Bluefruit");
+        while (1) {
+            delay(10);
+        }
+    } else {
+        Serial.println("Bluefruit started");
+    }
+
     Bluefruit.setTxPower(4);
-    Bluefruit.Periph.setConnectCallback(connect_callback);
-    Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
+    Bluefruit.Periph.setConnectCallback(connectCallback);
+    Bluefruit.Periph.setDisconnectCallback(disconnectCallback);
 
-    robotService.begin();
+    if (robotService.begin() != ERROR_NONE) {
+        Serial.println("Error starting robotService");
+        while (1) {
+            delay(10);
+        }
+    } else {
+        Serial.println("robotService started");
+    }
 
     commandCharacteristic.setProperties(CHR_PROPS_WRITE);
     commandCharacteristic.setPermission(SECMODE_NO_ACCESS, SECMODE_OPEN);
     commandCharacteristic.setFixedLen(1);
-    commandCharacteristic.setWriteCallback(command_callback);
-    commandCharacteristic.begin();
+    commandCharacteristic.setWriteCallback(commandCallback);
+
+    if (commandCharacteristic.begin() != ERROR_NONE) {
+        Serial.println("Error starting commandCharacteristic");
+        while (1) {
+            delay(10);
+        }
+    } else {
+        Serial.println("commandCharacteristic started");
+    }
 
     notificationCharacteristic.setProperties(CHR_PROPS_NOTIFY);
     notificationCharacteristic.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
     notificationCharacteristic.setFixedLen(1);
-    notificationCharacteristic.begin();
+
+    if (notificationCharacteristic.begin() != ERROR_NONE) {
+        Serial.println("Error starting notificationCharacteristic");
+        while (1) {
+            delay(10);
+        }
+    } else {
+        Serial.println("notificationCharacteristic started");
+    }
 
     Bluefruit.Advertising.addService(robotService);
     Bluefruit.Advertising.start(0);
 }
 
+void connectCallback(uint16_t conn_handle)
+{
+    Serial.println("Connected");
+}
+
+void disconnectCallback(uint16_t conn_handle, uint8_t reason)
+{
+    Serial.println("Disconnected");
+}
+
+void commandCallback(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, uint16_t len)
+{
+    Serial.println("Command received");
+
+    if (len == 1) {
+        if (data[0] == commandForward) {
+            leftMotor.throttle(0.3);
+            rightMotor.throttle(0.3);
+            delay(800);
+            leftMotor.throttle(0);
+            rightMotor.throttle(0);
+            notificationCharacteristic.notify8(notificationCommandFinished);
+        } else if (data[0] == commandLeft) {
+            leftMotor.throttle(-0.3);
+            rightMotor.throttle(0.3);
+            delay(472);
+            leftMotor.throttle(0);
+            rightMotor.throttle(0);
+            notificationCharacteristic.notify8(notificationCommandFinished);
+        } else if (data[0] == commandRight) {
+            leftMotor.throttle(0.3);
+            rightMotor.throttle(-0.3);
+            delay(472);
+            leftMotor.throttle(0);
+            rightMotor.throttle(0);
+            notificationCharacteristic.notify8(notificationCommandFinished);
+        }
+    }
+
+    Serial.println("Command completed");
+}
+
 void loop()
+{
+    // logAccelerometer();
+}
+
+void logAccelerometer()
 {
     Serial.print("motionX: ");
     Serial.print(CircuitPlayground.motionX());
