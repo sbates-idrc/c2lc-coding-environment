36a27ab9646df6f2bcb467d5146034a4172d2390
Simon Bates <sbates@ocadu.ca>
2024-03-07 09:22:42 -0500

    Set version to 1.10

--- src/App.js
+++ src/App.js
@@ -167,7 +167,7 @@ export class App extends React.Component<AppProps, AppState> {
     constructor(props: any) {
         super(props);
 
-        this.version = '1.9';
+        this.version = '1.10';
 
         this.appContext = {
             bluetoothApiIsAvailable: FeatureDetection.bluetoothApiIsAvailable()
