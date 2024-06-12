3d30e0a83235321c55e5a5ae59d594328ba2860b
Simon Bates <sbates@ocadu.ca>
2024-06-12 14:15:39 -0400

    Add a comment to scripts/eslint.js

--- scripts/eslint.js
+++ scripts/eslint.js
@@ -1,3 +1,6 @@
+// This script runs 'eslint' with the BABEL_ENV environment variable
+// set, as needed by babel-preset-react-app
+
 const spawn = require('child_process').spawn;
 
 process.env.BABEL_ENV = 'development';
